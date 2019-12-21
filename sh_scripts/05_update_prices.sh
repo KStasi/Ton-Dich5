a8c0d1c34dcc9c2f82dda9f0ec926d77096fb6f8
kstasi <nastasiakondaurova@gmail.com>
2019-12-21 16:46:51 +0200

    fixes

--- sh_scripts/05_update_prices.sh
+++ sh_scripts/05_update_prices.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 fift -s fift_scripts/update-prices.fif
-fift -s fift_scripts/wallet.fif "build/new-wallet" "0f8wr3wp7mKbKpOhJfpl4RUiMbyovUWZc_neAnH2H0LZvBLQ" 3 1 "./build/wallet-query" -B "./build/update-prices.boc"
-screen -S lite-client -p 0 -X stuff "sendfile ./build/wallet-query.boc
+screen -S lite-client -p 0 -X stuff "sendfile ./build/update-prices-query.boc
 "
