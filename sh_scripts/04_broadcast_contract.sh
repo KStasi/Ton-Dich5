363e5790c6f1ddedbf68bee74f63ba9febbaf53f
kstasi <nastasiakondaurova@gmail.com>
2019-12-21 17:24:42 +0200

    smc fixes

--- sh_scripts/04_broadcast_contract.sh
+++ sh_scripts/04_broadcast_contract.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-fift -s fift_scripts/wallet.fif "build/new-wallet" "0f8wr3wp7mKbKpOhJfpl4RUiMbyovUWZc_neAnH2H0LZvBLQ" 1 0.5 "./build/wallet-query"
+fift -s fift_scripts/wallet.fif "build/new-wallet" "0f8Gl0Pc2yYkxNzWOTU8f6DOsdY32_2SEma705D7cVK3DptE" 4 0.5 "./build/wallet-query"
 screen -S lite-client -p 0 -X stuff "sendfile ./build/wallet-query.boc
 "
 sleep 5
