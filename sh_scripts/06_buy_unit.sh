729a9299ff333edb51ce5537fc2cc58870279312
kstasi <nastasiakondaurova@gmail.com>
2019-12-22 12:54:56 +0200

    buy updated

--- sh_scripts/06_buy_unit.sh
+++ sh_scripts/06_buy_unit.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
-CONTRACT=`fift -s fift_scripts/show-addr.fif build/new-game | grep "Non-bounceable address (for init only):" | cut -d ' ' -f6`
-fift -s fift_scripts/buy-unit.fif
-fift -s fift_scripts/wallet.fif "build/new-wallet" $CONTRACT 15 0.5 "./build/wallet-query" -B "./build/buy-unit.boc"
+CONTRACT=`fift -s fift_scripts/show-bouceable-addr.fif build/new-game `
+fift -s fift_scripts/buy-unit.fif 10001 1 8
+fift -s fift_scripts/wallet.fif "build/new-wallet" $CONTRACT 16 0.05 "./build/wallet-query" -B "./build/buy-unit.boc"
+screen -S lite-client -p 0 -X stuff "sendfile ./build/wallet-query.boc
+ "
+sleep 3
+
+fift -s fift_scripts/buy-unit.fif 20002 2 8
+fift -s fift_scripts/wallet.fif "build/new-wallet" $CONTRACT 17 0.05 "./build/wallet-query" -B "./build/buy-unit.boc"
+screen -S lite-client -p 0 -X stuff "sendfile ./build/wallet-query.boc
+ "
+sleep 3
+
+fift -s fift_scripts/buy-unit.fif 30003 3 8
+fift -s fift_scripts/wallet.fif "build/new-wallet" $CONTRACT 18 0.05 "./build/wallet-query" -B "./build/buy-unit.boc"
+screen -S lite-client -p 0 -X stuff "sendfile ./build/wallet-query.boc
+ "
+sleep 3
+
+fift -s fift_scripts/buy-unit.fif 40004 4 8
+fift -s fift_scripts/wallet.fif "build/new-wallet" $CONTRACT 19 0.05 "./build/wallet-query" -B "./build/buy-unit.boc"
+screen -S lite-client -p 0 -X stuff "sendfile ./build/wallet-query.boc
+ "
+sleep 3
+
+fift -s fift_scripts/buy-unit.fif 50005 5 8
+fift -s fift_scripts/wallet.fif "build/new-wallet" $CONTRACT 20 0.05 "./build/wallet-query" -B "./build/buy-unit.boc"
 screen -S lite-client -p 0 -X stuff "sendfile ./build/wallet-query.boc
  "
