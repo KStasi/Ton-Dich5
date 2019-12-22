68cc7708cb79f455447095dd39ec1f0559d544cd
kstasi <nastasiakondaurova@gmail.com>
2019-12-22 12:16:47 +0200

    names updated

--- sh_scripts/07_line_up.sh
+++ sh_scripts/07_line_up.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CONTRACT=`fift -s fift_scripts/show-addr.fif build/new-game | grep "Non-bounceable address (for init only):" | cut -d ' ' -f6`
 fift -s fift_scripts/line-up-queue.fif
-fift -s fift_scripts/wallet.fif "build/new-wallet" $CONTRACT 15 0.5 "./build/wallet-query" -B "./build/buy-unit.boc"
+fift -s fift_scripts/wallet.fif "build/new-wallet" $CONTRACT 15 0.5 "./build/wallet-query" -B "./build/line-up.boc"
 screen -S lite-client -p 0 -X stuff "sendfile ./build/wallet-query.boc
  "
