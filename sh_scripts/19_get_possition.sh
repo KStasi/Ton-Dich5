fe34ef313f38dd8e5a3d56a5309c191284f7c13e
kstasi <nastasiakondaurova@gmail.com>
2020-03-22 19:08:38 +0200

    get possition method added

--- sh_scripts/19_get_possition.sh
+++ sh_scripts/19_get_possition.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-CONTRACT="kf-tRnWNAZDD8D3LNqFuSty8BIantwY2KqI6FBmo3Ac-yIfv" #`fift -s fift_scripts/show-bouceable-addr.fif build/new-game `
+CONTRACT="kf_5GvwIvNdaWMX8STxrx1bULLf1eUvpTyatEuooSRTWyAeP" #`fift -s fift_scripts/show-bouceable-addr.fif build/new-game `
 ./lite-client/lite-client -C ./lite-client/ton-global.config -l null -c 'saveaccountdata ./build/contract.boc '$CONTRACT
 ./sh_scripts/03_create_contract.sh
-fift -s fift_scripts/get-locations.fif $CONTRACT 1
+func -P -o build/reader-code.fif contracts/stdlib.fc contracts/reader-code.fc
+fift -s fift_scripts/get-locations.fif $CONTRACT 1 2> /dev/null
